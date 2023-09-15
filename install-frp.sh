5994ecbee09cfbd85248fa220460b25bc4627686
j8 <o@o>
2023-09-16 00:28:58 +0800

    frp53

--- install-frp.sh
+++ install-frp.sh
@@ -6,7 +6,7 @@ mkdir -p frp
 pushd frp
 
 if [ ! -x frps ]; then
-    wget -qOfrp.tgz https://github.com/fatedier/frp/releases/download/v0.34.2/frp_0.34.2_linux_amd64.tar.gz
+    wget -qOfrp.tgz https://github.com/fatedier/frp/releases/download/v0.51.3/frp_0.51.3_linux_amd64.tar.gz
     tar xf frp.tgz --strip-components=1
 fi
 
